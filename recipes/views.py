ff07852cc9b16e74ed27262d44bcfaf30dc7f659
Thomás Serra <thgserra@gmail.com>
2022-05-03 12:42:29 -0300

    Alguns Fixes

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 
+from .models import Recipe
+
 
 def home(request):
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
     })
 
 
@@ -13,4 +18,13 @@ def recipe(request, id):
         'recipe': make_recipe(),
         'is_detail_page': True,
     })
-# Create your views here.
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True
+    ).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+    })
